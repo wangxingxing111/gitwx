57e005833d04946135db25ffbdfb15309cfb6c65
wangxingxing111 <2410907095@qq.com>
2019-10-14 16:55:41 +0800

    Initial commitaaaaa

---
+++ src/main/java/com/example/gitwx/gitwx/GitwxApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitwx.gitwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitwxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitwxApplication.class, args);
+    }
+
+}
